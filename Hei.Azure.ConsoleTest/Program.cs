215e0ac4242df74ed7946e277d03521852111cb1
wangnima <389483637@qq.com>
2022-08-31 01:07:34 +0800

    语音识别

--- Hei.Azure.ConsoleTest/Program.cs
+++ Hei.Azure.ConsoleTest/Program.cs
@@ -1,83 +1,54 @@
-﻿using Azure.Messaging.EventGrid;
-using Microsoft.Azure.ServiceBus;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.AzureAppConfiguration;
-using Microsoft.Extensions.Configuration.AzureAppConfiguration.Extensions;
-using Microsoft.Extensions.Configuration.Json;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
 
 class Program
 {
-    private const string AppConfigurationConnectionStringEnvVarName = "AppConfigurationConnectionString";
-    // e.g. Endpoint=https://{store_name}.azconfig.io;Id={id};Secret={secret}
+    static string YourSubscriptionKey = "f83702bb4db7493fa9bea859e8d525ce";
+    static string YourServiceRegion = "chinaeast2";
 
-    private const string ServiceBusConnectionStringEnvVarName = "ServiceBusConnectionString";
-    // e.g. Endpoint=sb://{service_bus_name}.servicebus.windows.net/;SharedAccessKeyName={key_name};SharedAccessKey={key}
-
-    private const string ServiceBusTopicEnvVarName = "ServiceBusTopic";
-    private const string ServiceBusSubscriptionEnvVarName = "ServiceBusSubscription";
-
-    private static IConfigurationRefresher _refresher = null;
-
-    static async Task Main(string[] args)
+    async static Task Main(string[] args)
     {
-        string appConfigurationConnectionString = Environment.GetEnvironmentVariable(AppConfigurationConnectionStringEnvVarName);
+        var speechConfig = SpeechConfig.FromSubscription(YourSubscriptionKey, YourServiceRegion);
+        speechConfig.SpeechRecognitionLanguage = "zh-CN";
 
-        IConfiguration configuration = new ConfigurationBuilder()
-            .AddAzureAppConfiguration(options =>
-            {
-                options.Connect(appConfigurationConnectionString);
-                options.ConfigureRefresh(refresh =>
-                    refresh
-                        .Register("TestApp:Settings:Message")
-                        .SetCacheExpiration(TimeSpan.FromDays(10))  // Important: Reduce poll frequency
-                );
+        //识别麦克风的输入
+        //using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+        //
+        using var audioConfig = AudioConfig.FromWavFileInput();
 
-                _refresher = options.GetRefresher();
-            }).Build();
+        using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
 
-        RegisterRefreshEventHandler();
-        var message = configuration["TestApp:Settings:Message"];
-        Console.WriteLine($"Initial value: {configuration["TestApp:Settings:Message"]}");
-
-        while (true)
-        {
-            await _refresher.TryRefreshAsync();
-
-            if (configuration["TestApp:Settings:Message"] != message)
-            {
-                Console.WriteLine($"New value: {configuration["TestApp:Settings:Message"]}");
-                message = configuration["TestApp:Settings:Message"];
-            }
-
-            await Task.Delay(TimeSpan.FromSeconds(1));
-        }
+        Console.WriteLine("Speak into your microphone.");
+        var speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
+        OutputSpeechRecognitionResult(speechRecognitionResult);
     }
 
-    private static void RegisterRefreshEventHandler()
+    static void OutputSpeechRecognitionResult(SpeechRecognitionResult speechRecognitionResult)
     {
-        string serviceBusConnectionString = Environment.GetEnvironmentVariable(ServiceBusConnectionStringEnvVarName);
-        string serviceBusTopic = Environment.GetEnvironmentVariable(ServiceBusTopicEnvVarName);
-        string serviceBusSubscription = Environment.GetEnvironmentVariable(ServiceBusSubscriptionEnvVarName);
-        SubscriptionClient serviceBusClient = new SubscriptionClient(serviceBusConnectionString, serviceBusTopic, serviceBusSubscription);
+        switch (speechRecognitionResult.Reason)
+        {
+            case ResultReason.RecognizedSpeech:
+                Console.WriteLine($"RECOGNIZED: Text={speechRecognitionResult.Text}");
+                break;
 
-        serviceBusClient.RegisterMessageHandler(
-            handler: (message, cancellationToken) =>
-            {
-                // Build EventGridEvent from notification message
-                EventGridEvent eventGridEvent = EventGridEvent.Parse(BinaryData.FromBytes(message.Body));
+            case ResultReason.NoMatch:
+                Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                break;
 
-                // Create PushNotification from eventGridEvent
-                eventGridEvent.TryCreatePushNotification(out PushNotification pushNotification);
+            case ResultReason.Canceled:
+                var cancellation = CancellationDetails.FromResult(speechRecognitionResult);
+                Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
 
-                // Prompt Configuration Refresh based on the PushNotification
-                _refresher.ProcessPushNotification(pushNotification);
-
-                return Task.CompletedTask;
-            },
-            exceptionReceivedHandler: (exceptionargs) =>
-            {
-                Console.WriteLine($"{exceptionargs.Exception}");
-                return Task.CompletedTask;
-            });
+                if (cancellation.Reason == CancellationReason.Error)
+                {
+                    Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                    Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                    Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
+                }
+                break;
+        }
     }
 }
